39578d68fa2992958ad3a8a37d13ba3585718c67
moxier <2602936348@qq.com>
2019-03-11 15:13:38 +0800

    修改访问loclahost

--- Books/src/router/index.js
+++ Books/src/router/index.js
@@ -18,75 +18,62 @@ Vue.use(Router);
 export default new Router({
   mode: "history",
   routes: [{
-      path: "/",
-      name: "Home",
-      component: Home
-    },
-    {
-      path: "/bookshelf/my",
-      name: "BookShelf",
-      component: BookShelf
-    },
-    {
-      path: "/bookshelf/my-history",
-      name: "BookShelfHistory",
-      component: BookShelfHistory
-    },
-    {
-      path: "/search",
-      name: "SearchResult",
-      component: SearchResult,
-      children: [{
-        path: ":kw"
-      }]
-    },
-    {
-      path: "/book/:id",
-      name: "BookDeatils",
-      component: BookDeatils,
-    },
-    {
-      path: '/book/:id/catalog',
-      name: 'BookChapter',
-      component: BookChapter
-    },
-    {
-      path: '/book/:id/read',
-      name: 'BookContent',
-      component: BookContent
-    },
-    {
-      path: '/catrgory',
-      name: 'Catrgory',
-      component: Catrgory,
-    },
-    {
-      path: '/catrgory/details',
-      name: 'CategoryDetails',
-      component: CategoryDetails,
-      children: [{
-        path: ':gender&:type&:major&:minor&:start&:limit'
-      }]
-    },
-    {
-      path: '/ranking/:gender',
-      name: 'Ranking',
-      component: Ranking,
-    },
-    {
-      path: '/ranking/:id/:gender',
-      name: 'RankingDetail',
-      component: RankingDetail
-    },
-    {
-      path: '/test',
-      name: "GoTop",
-      component: GoTop
-    },
-    {
-      path: "*",
-      name: "Error",
-      component: Errors
-    }
-  ]
+    path: "/",
+    name: "Home",
+    component: Home
+  }, {
+    path: "/bookshelf/my",
+    name: "BookShelf",
+    component: BookShelf
+  }, {
+    path: "/bookshelf/my-history",
+    name: "BookShelfHistory",
+    component: BookShelfHistory
+  }, {
+    path: "/search",
+    name: "SearchResult",
+    component: SearchResult,
+    children: [{
+      path: ":kw"
+    }]
+  }, {
+    path: "/book/:id",
+    name: "BookDeatils",
+    component: BookDeatils,
+  }, {
+    path: '/book/:id/catalog',
+    name: 'BookChapter',
+    component: BookChapter
+  }, {
+    path: '/book/:id/read',
+    name: 'BookContent',
+    component: BookContent
+  }, {
+    path: '/catrgory',
+    name: 'Catrgory',
+    component: Catrgory,
+  }, {
+    path: '/catrgory/details',
+    name: 'CategoryDetails',
+    component: CategoryDetails,
+    children: [{
+      path: ':gender&:type&:major&:minor&:start&:limit'
+    }]
+  }, {
+    path: '/ranking/:gender',
+    name: 'Ranking',
+    component: Ranking,
+  }, {
+    path: '/ranking/:id/:gender',
+    name: 'RankingDetail',
+    component: RankingDetail
+  }, {
+    path: '/test',
+    name: "GoTop",
+    component: GoTop
+  }, {
+    path: "*",
+    name: "Error",
+    component: Errors
+  }]
 });
